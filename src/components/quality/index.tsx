9bcbf8063e050670c1b925416aebab36e7d07de5
reggiegunawan88 <reggiegunawan88@gmail.com>
2022-03-22 17:16:29 +0700

    fix(landing-page): adjust grid and line height for quality sec

--- src/components/quality/index.tsx
+++ src/components/quality/index.tsx
@@ -7,36 +7,64 @@ const index = () => {
       <p className="title-text">Quality</p>
       <p className="subtitle-text">We provide professional and high-quality customer services</p>
       {/* quality ilustration */}
-      <div className="grid grid-cols-3 gap-x-20 mt-5">
+      <div className="grid grid-cols-3 gap-x-8 mt-5">
         {/* quality 1 */}
-        <div className="flex flex-col gap-y-5 leading-8">
-          <Image alt="quality-1" src="/assets/quality/iso-1.png" width={313} height={313} />
-          <p className="text-3xl font-semibold ">
-            <span className="text-greenCV">Professional Legit </span>Checker
+        <div className="flex flex-col gap-y-5">
+          <Image
+            alt="quality-1"
+            src="/assets/quality/iso-1.png"
+            width={313}
+            height={313}
+            layout="fixed"
+          />
+          <p className="text-3xl font-semibold leading-8">
+            <span className="text-greenCV">
+              Professional <br />{' '}
+            </span>
+            Legit Checker
           </p>
-          <p className="text-2xl">Checkervisor team is build by the expert and high experiences in sneakers &amp; streetwear</p>
+          <p className="text-2xl leading-6">
+            Checkervisor team is build by the expert and high experiences in sneakers &amp;
+            streetwear
+          </p>
         </div>
         {/* quality 2 */}
-        <div className="flex flex-col gap-y-5 leading-8">
-          <Image alt="quality-1" src="/assets/quality/iso-2.png" width={313} height={313} />
-          <p className="text-3xl font-semibold ">
+        <div className="flex flex-col gap-y-5">
+          <Image
+            alt="quality-1"
+            src="/assets/quality/iso-2.png"
+            width={313}
+            height={313}
+            layout="fixed"
+          />
+          <p className="text-3xl font-semibold leading-8">
             <span className="text-greenCV">
               Organized <br />
             </span>
             &amp; Well Built System
           </p>
-          <p className="text-2xl">Your item was legit checked in order by passing our standarization of authenticity</p>
+          <p className="text-2xl leading-6">
+            Your item was legit checked in order by passing our standarization of authenticity
+          </p>
         </div>
         {/* quality 3 */}
-        <div className="flex flex-col gap-y-5 leading-8">
-          <Image alt="quality-1" src="/assets/quality/iso-3.png" width={313} height={313} />
-          <p className="text-3xl font-semibold ">
+        <div className="flex flex-col gap-y-5">
+          <Image
+            alt="quality-1"
+            src="/assets/quality/iso-3.png"
+            width={313}
+            height={313}
+            layout="fixed"
+          />
+          <p className="text-3xl font-semibold leading-8 whitespace-nowrap">
             <span className="text-greenCV">
               TrustWorthy <br />
             </span>
             Education &amp; Explanation
           </p>
-          <p className="text-2xl">Your authentication included 100% TrustWorthy from the expert now</p>
+          <p className="text-2xl leading-6">
+            Your authentication included 100% TrustWorthy from the expert now
+          </p>
         </div>
       </div>
     </div>
